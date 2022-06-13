9d8bfac860a7cba58475e7b9b2249c283e95e42e
elizabethvera-atieno <atienolizvera@gmail.com>
2022-06-13 14:09:21 +0300

    commit tree f89622420cd0c942d69731fdb781d26c4b14edc6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize side1, side2, side3
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    array_size = @sides.uniq.size
+    if(@sides.include? 0)
+      raise TriangleError
+    end
+
+    if is_valid_triangle?
+      if array_size == 1
+        return :equilateral
+      elsif array_size == 2
+        return :isosceles
+      elsif array_size == 3
+        return :scalene
+      elsif array_size == 0
+        raise TriangleError
+      end
+    else
+      raise TriangleError
+    end
+    
+    
+  end
+
+  class TriangleError < StandardError
+
+  end
+
+  def is_valid_triangle?
+    sorted_lenghts = [side1, side2, side3].sort
+    longest = sorted_lenghts.pop
+    longest < sorted_lenghts.sum
+  end
+
+
 end
